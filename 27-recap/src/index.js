4723e9e160e96e83593406fcc88a0e28d3ab4724
Maxcell Wilson <maxcell.wilson@gmail.com>
2018-11-26 17:44:30 -0500

    Update to include finished code

--- 27-recap/src/index.js
+++ 27-recap/src/index.js
@@ -29,9 +29,17 @@ someResponse.then(response => response.json())
 })
 
 // - Make a dog editable. Clicking on the edit button next to a dog should populate the top form with that dog's current information.
+// We need an event listener
+// We can either add it on the Edit button
+// Or on the table
+
 document.addEventListener('DOMContentLoaded', (e) =>{
+  let dogForm = document.getElementById('dog-form')
   const tableBody = document.getElementById('table-body')
 
+  // When going through the approach of adding to the table
+  // I need to know WHICH edit button I'm clicking
+  // We can add a data-id
   tableBody.addEventListener('click', (e) => {
     // We need some conditional to check if we are clicking on an edit button
     if(e.target.className === "dog-edit"){
@@ -40,42 +48,76 @@ document.addEventListener('DOMContentLoaded', (e) =>{
       .then(json => {
         // Finding the form
         // Filling in the values
-        let dogForm = document.getElementById('dog-form')
-        debugger
+        // **** This is where we ended ****
+        // We are going to need to populate the form
+        dogForm.elements[0].value = json.name
+        dogForm.elements[1].value = json.breed
+        dogForm.elements[2].value = json.sex
+        dogForm.dataset.id = json.id // I want this for the patch
       })
     }
   })
-  // console.log(tableBody)
+
+  // *** Added this after lecture
+  // Extrapolated the dogForm out of the event listener to reuse HTML
+  dogForm.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    // ******** YOU CAN ASSUME YOUR CODE WORKS BUT I DID THIS TOTALLY AS
+    // A WAY TO PREVENT PREEMPTIVE SUBMISSIONS
+    // debugger
+    if(e.target.dataset.id === undefined) { alert('select a dog');return }
+    // ******* I AM ONLY ADDING THIS CONDITIONAL TO AVOID THE CODE TO EXECUTE BELOW
+
+
+
+    // - On submit of the form, a PATCH request should be made to http://localhost:3000/dogs/:id to update the dog information (including name, breed and sex attributes).
+    fetch(`http://localhost:3000/dogs/${e.target.dataset.id}`, {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "name": dogForm.elements[0].value, // name
+        "breed": dogForm.elements[1].value, // breed
+        "sex": dogForm.elements[2].value // sex
+      })
+    })
+    .then(res => res.json())
+    .then(json => {
+      // Now our form has submitted
+      // Reset the form ** also not necessary **
+      dogForm.reset()
+
+      // How can we update the page???
+      // 1. We either reload the entire table
+      // Basically means we do exactly the same logic as we loaded the entire page
+      fetch("http://localhost:3000/dogs").then(response => response.json())
+      .then(function(dogs){
+        const tableBody = document.getElementById('table-body')
+        /// *** ONLY CHANGE BETWEEN WHAT IS ABOVE IN OUR INITIAL RENDER
+        /// AND HERE is innerhtml being set back to "" before adding
+        tableBody.innerHTML = ""
+        dogs.forEach(dog => {
+          tableBody.innerHTML += `<tr><td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button class="dog-edit" data-id="${dog.id}">Edit</button></td></tr>`
+        })
+      })
+
+      // 2. WE UPDATE THE DOM SPECIFICALLY TO THE ONE WE WANT
+      // - We find the particular row (we could use the id of our JSON to find the button with the matching data-id)
+      // - update each column appropriately
+      // SINCE I PUT THE DATA-ID ON THE BUTTON, I HAVE TO SELECT THE GRANDPARENTELEMENT
+      // let dogRow = document.querySelector(`button[data-id="${json.id}"]`).parentElement.parentElement
+      //
+      // dogRow.children[0].innerText = json.name
+      // dogRow.children[1].innerText = json.breed
+      // dogRow.children[2].innerText = json.sex
+    })
+  })
+
 })
 
 
-// We need an event listener
-// We can either add it on the Edit button
-// Or on the table
-
-// When going through the approach of adding to the table
-
-
-// I need to know WHICH edit button I'm clicking
-// We can add an id
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// - On submit of the form, a PATCH request should be made to http://localhost:3000/dogs/:id to update the dog information (including name, breed and sex attributes).
-// - Once the form is submitted, the table should reflect the updated dog information. There are many ways to do this. You could search for the table fields you need to edit and update each of them in turn, but we suggest making a new get request for all dogs and rerendering all of them in the table. Make sure this GET happens after the PATCH so you can get the most up-to-date dog information.
 
 
 // How to debug: debugger
