6455aa85df4c8c94e6d29d24673de96fc0e8c257
github username <github email address>
2018-12-12 10:08:42 -0500

    more practice time

--- 33-react-state-props/rap-rater/src/Components/Rapper.js
+++ 33-react-state-props/rap-rater/src/Components/Rapper.js
@@ -1,30 +1,11 @@
 import React from "react";
 
 class Rapper extends React.Component {
-  state = {
-    flip: true
-  };
-
-  handleClick = () => {
-    this.setState({
-      flip: !this.state.flip
-    });
-  };
-
   render() {
     return (
       <div>
         <h2>{this.props.rapper.name}</h2>
-        <img
-          onClick={this.handleClick}
-          src={
-            this.state.flip
-              ? this.props.rapper.happyImage
-              : this.props.rapper.sadImage
-          }
-          alt=""
-          className="rapper"
-        />
+        <img src={this.props.rapper.sadImage} alt="" className="rapper" />
       </div>
     );
   }
